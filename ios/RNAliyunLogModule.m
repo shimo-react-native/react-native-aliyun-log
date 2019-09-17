df40fdf7122abb8312e192f426c865552a711140
钟加仁 <zhongjiaren@shimo.im>
2019-09-17 10:59:01 +0800

    chore: update podspec file

--- ios/RNAliyunLogModule.m
+++ ios/RNAliyunLogModule.m
@@ -1,5 +1,5 @@
 #import "RNAliyunLogModule.h"
-#import <AliyunLogObjc/AliyunLogObjc.h>
+#import "AliyunLogObjc.h"
 
 @interface RNAliyunLogModule ()
 
@@ -12,15 +12,16 @@
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(initAliyunLog:(NSString*)endPoint accessKeyId:(NSString*)accessKeyId accessKeySecret:(NSString*)accessKeySecret securityToken:(NSString*)securityToken project:(NSString*)project) {
-  _logClient = [[LogClient alloc] initWithApp:endPoint accessKeyID:accessKeyId accessKeySecret:accessKeySecret projectName:project];
+    
+      _logClient = [[LogClient alloc] initWithApp:endPoint accessKeyID:accessKeyId accessKeySecret:accessKeySecret projectName:project serializeType: AliSLSJSONSerializer];
   if (securityToken) {
     [_logClient SetToken:securityToken];
   }
 }
 
 RCT_EXPORT_METHOD(postAliyunLog:(NSString*)logStore topic:(NSString*)topic source:(NSString*)source keyContents:(NSArray*)keyContents) {
-  LogGroup *group = [[LogGroup alloc] initWithTopic:topic andSource:source];
-  Log *log = [[Log alloc] init];
+  RawLogGroup *group = [[RawLogGroup alloc] initWithTopic:topic andSource:source];
+  RawLog *log = [[RawLog alloc] init];
   [keyContents enumerateObjectsUsingBlock:^(NSDictionary *keyContent, NSUInteger idx, BOOL * _Nonnull stop) {
     [log PutContent:[keyContent objectForKey:@"content"] withKey:[keyContent objectForKey:@"key"]];
   }];
